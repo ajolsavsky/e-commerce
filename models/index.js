b042120e5c0e8521aed727c830475251c5359f96
Alexandra Olsavsky <alexandraolsavsky@gmail.com>
2022-06-29 11:13:34 -0500

    added relationships in index.js in models

--- models/index.js
+++ models/index.js
@@ -5,12 +5,34 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  //Foreign Key
+  //On delete
+})
 
 // Categories have many Products
+Category.hasMany(Product, {
+  //Foreign Key
+  //On delete
+})
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+  as: 'product_tags'
+})
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+  as: 'tags_for_products'
+})
 
 module.exports = {
   Product,
