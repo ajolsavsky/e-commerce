13cf0d232533d513b21acdb972ed584b79c1d4e9
Alexandra Olsavsky <alexandraolsavsky@gmail.com>
2022-07-06 23:58:15 -0500

    routes fully functional

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -34,15 +34,50 @@ router.get('/:id', async (req, res) => {
   // be sure to include its associated Product data
 });
 
-router.post('/', (req, res) => {
-  // create a new tag
+router.post('/', async (req, res) => {
+  try {
+    const tagData = await Tag.create(req.body);
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
-router.put('/:id', (req, res) => {
-  // update a tag's name by its `id` value
+router.put('/:id', async (req, res) => {
+  try {
+    const tagData = await Tag.update(
+      {
+        "tag_name": req.body.tag_name,
+      },
+      {
+        where: {
+          id: req.params.id,
+        }
+      }
+    );
+    if (!tagData) {
+      res.status(500).json("Tag is not found")
+    }
+    return res.status(200).json({ message: 'Tag has been updated!'});
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
+  try {
+    const tagData = await Tag.destroy({
+      where: {
+        id: req.params.id,
+      }
+    });
+    if (!tagData) {
+      res.status(404).json({ message: 'No tag found with this id'})
+    }
+    res.status(200).json({ message: 'Tag has been deleted!'});
+  } catch (err) {
+    res.status(500).json(err);
+  }
   // delete on tag by its `id` value
 });
 
