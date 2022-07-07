0a2574e2ae3997515f78e2ac4d7511f214ad98c8
Alexandra Olsavsky <alexandraolsavsky@gmail.com>
2022-07-06 21:51:41 -0500

    categories all and one GET functional

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,12 +3,33 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
+  try {
+    const categoryData = await Category.findAll({
+      include: [{ model: Product }]
+    });
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+  
   // find all categories
   // be sure to include its associated Products
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
+  try {
+    const categoryData = await Category.findByPk(req.params.id, {
+      include: [{ model: Product }]
+    });
+    if (!categoryData) {
+      res.status(500).json("Category is not found")
+    }
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+  
   // find one category by its `id` value
   // be sure to include its associated Products
 });
